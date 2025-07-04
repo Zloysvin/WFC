67346a35301c27037e3dfc2df53b3f886e1a25bd
Zloysvin <danylomartakov@gmail.com>
2025-07-03 17:57:35 -0700

    Project setup

---
+++ WFC/ImageHelper.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Drawing;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WFC
+{
+    public static class ImageHelper
+    {
+        public static List<Bitmap> GetSplittedBitmaps()
+        {
+            Bitmap main = ReadImage();
+        }
+
+        private static Bitmap ReadImage()
+        {
+            Console.WriteLine("Enter Base Image Path:");
+            string path = @Console.ReadLine(); // TODO: add checks for valid paths
+
+            return new Bitmap(path);
+        }
+    }
+}
